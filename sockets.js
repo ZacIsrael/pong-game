fc91f7391ba63654e485a9b1fba4b3fe1ecd41b8
ZacIsrael <zac390@gmail.com>
2022-08-03 00:43:44 -0400

    Incorporated socket io rooms in order to allow multiple games to go on at once

--- sockets.js
+++ sockets.js
@@ -10,12 +10,21 @@ function listen(io){
     // listen to the connection event, 
     // server will recieve this event everytime a user/client connects to it 
     pongNameSpace.on('connection', (socket) => {
+        // rooms group players into pairs
+        let room;
         // socket used to communicate with the user/client that just connected 
         console.log(`User/client connected. socket.id= ${socket.id}`);
 
         // listen for the ready event
         socket.on('ready', () => {
-            console.log(`User/client with socket.id= ${socket.id} is ready to play`);
+     
+            // the first & second users/clients will join room 0 (0/2 = 0, 1/2 = 0),
+            // the 3rd and 4th users/clients will join room 1 (2/2 = 1, 3/2 = 1) and so on
+            room = 'room' + Math.floor(readyPlayerCount/2);
+            // the current client/user joins the room
+            socket.join(room);
+
+            console.log(`User/client with socket.id= ${socket.id} is ready to play\n They are in room ${room}\n`);
 
             readyPlayerCount++;
 
@@ -24,23 +33,24 @@ function listen(io){
                 // start the game
                 // broadcast to both clients that the game is ready
 
-                // lets all the clients know that the game is ready
+                // lets all the clients in the room know that the game is ready
                 // second paramater: id of the client that will be the referee. In this case, it will always be the second player  
-                pongNameSpace.emit('startGame', socket.id)
+                pongNameSpace.in(room).emit('startGame', socket.id)
+
             }
         });
 
         socket.on('paddleMove', (paddleData) => {
             // paddleData object contains the x position of the current player's paddle 
 
-            // send this data to the other player 
-            socket.broadcast.emit('paddleMove', paddleData)
+            // send this data to the other player in the room  
+            socket.to(room).emit('paddleMove', paddleData)
         });
 
         // listen for when the ball has moved 
         socket.on('ballMove', (ballData) => {
-            // broadcast the x & y coordinates of the ball and the score to the opponent's browser
-            socket.broadcast.emit('ballMove', ballData)
+            // broadcast the x & y coordinates of the ball and the score to the opponent's browser (in the room)
+            socket.to(room).emit('ballMove', ballData)
         });
 
         // listens for when a user/client disconnects for whatever reason
@@ -48,6 +58,7 @@ function listen(io){
         socket.on('disconnect', (reason) => {
             // the reason parameter is the reason why that user/client disconnected 
             console.log(`User/client with socket.id=  ${socket.id} disconnected because ${reason}`)
+            socket.leave(room);
         });
     });
 }
